2f83149c846725436266ebcd1a7e62017e6740d2
lazorax <riyahere17@gmail.com>
2025-10-29 14:26:21 +0530

    deployment configuration

--- db.js
+++ db.js
@@ -2,11 +2,20 @@ import pg from "pg"
 import dotenv from "dotenv"
 
 dotenv.config()
+// console.log(process.env.DATABASE_URL);
 
 const db = new pg.Client({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
-db.connect()
+db.connect().then(() => console.log("Connected to Render PostgreSQL!"))
+  .catch(err => {
+    console.error("Database connection failed:");
+    console.error(err);
+    process.exit(1); // stop app if DB fails
+  });
 
 export default db;
